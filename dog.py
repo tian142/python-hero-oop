30f99a9125ffd005901e22dda0d83253807090dc
Tian Qiu <tian.qiu142@gmail.com>
2020-11-19 01:31:50 -0800

    first commit

---
+++ dog.py
@@ -0,0 +1,8 @@
+class Dog:
+    def __init__(self, name, breed):
+        self.name = name
+        self.breed = breed
+        print("dog initialized!")
+
+    def bark(self):
+        print("Woof!")
---
+++ my_dogs.py
@@ -0,0 +1,10 @@
+from dog import Dog
+
+my_dog = Dog("Rex", "SuperDog")
+my_dog.bark()
+
+my_other_dog = Dog("Annie", "SuperDog")
+print(my_other_dog.name)
+
+my_third_dog = Dog("Teemo", "Big Dog")
+print(my_third_dog.name)
