bd818961d9979280395fe75f602add027eee28f2
Tian Qiu <tian.qiu142@gmail.com>
2020-11-22 13:15:13 -0800

    completed part 3

--- heros/hero.py
+++ heros/hero.py
@@ -18,8 +18,27 @@ class Hero:
 
     def fight(self, opponent):
         heros = [self.name, opponent]
-        winnder = random.choice(heros)
-        print(f"{winnder} won!")
+        winner = random.choice(heros)
+        print(f"{winner} won!")
+
+
+class Ability:
+    def __init__(self, name, max_damage):
+        '''
+       Initialize the values passed into this
+       method as instance variables.
+        '''
+
+        # Assign the "name" and "max_damage"
+        # for a specific instance of the Ability class
+        self.name = name
+        self.max_damage = max_damage
+
+    def attack(self):
+        ''' Return a value between 0 and the value set by self.max_damage.'''
+        # Pick a random value between 0 and self.max_damage
+        random_value = random.randint(0, self.max_damage)
+        return random_value
 
 
 if __name__ == "__main__":
@@ -28,3 +47,7 @@ if __name__ == "__main__":
     print(my_hero.current_health)
 
     my_hero.fight('hero2')
+
+    ability = Ability("Debugging Ability", 20)
+    print(ability.name)
+    print(ability.attack())
