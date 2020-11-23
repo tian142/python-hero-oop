26bd5eb25ed40556bba12ec5f08b3c0234cc781a
Tian Qiu <tian.qiu142@gmail.com>
2020-11-22 17:24:47 -0800

    completed part 4

---
+++ heros/ability.py
@@ -0,0 +1,20 @@
+import random
+
+
+class Ability:
+    def __init__(self, name, max_damage):
+        '''
+       Initialize the values passed into this
+       method as instance variables.
+        '''
+
+        # Assign the "name" and "max_damage"
+        # for a specific instance of the Ability class
+        self.name = name
+        self.max_damage = max_damage
+
+    def attack(self):
+        ''' Return a value between 0 and the value set by self.max_damage.'''
+        # Pick a random value between 0 and self.max_damage
+        random_value = random.randint(0, self.max_damage)
+        return random_value
--- heros/hero.py
+++ heros/hero.py
@@ -1,3 +1,6 @@
+from ability import Ability
+from armor import Armor
+
 import random
 
 
@@ -5,49 +8,98 @@ class Hero:
     # We want our hero to have a default "starting_health",
     # so we can set that in the function header.
     def __init__(self, name, starting_health=100):
+        # abilities and armors don't have starting values,
+        # and are set to empty lists on initialization
+        self.abilities = list()
+        self.armors = list()
         # we know the name of our hero, so we assign it here
         self.name = name
-    # similarly, our starting health is passed in, just like name
+        # similarly, our starting health is passed in, just like name
         self.starting_health = starting_health
-    # when a hero is created, their current health is
-    # always the same as their starting health (no damage taken yet!)
+        # when a hero is created, their current health is
+        # always the same as their starting health (no damage taken yet!)
         self.current_health = starting_health
+        self.deaths = 0
+        self.kills = 0
 
     # If you run this file from the terminal
     # this block is executed.
 
-    def fight(self, opponent):
-        heros = [self.name, opponent]
-        winner = random.choice(heros)
-        print(f"{winner} won!")
+    def add_ability(self, ability):
+        self.abilities.append(ability)
 
-
-class Ability:
-    def __init__(self, name, max_damage):
-        '''
-       Initialize the values passed into this
-       method as instance variables.
-        '''
-
-        # Assign the "name" and "max_damage"
-        # for a specific instance of the Ability class
-        self.name = name
-        self.max_damage = max_damage
+    def add_armor(self, armor):
+        self.armors.append(armor)
 
     def attack(self):
-        ''' Return a value between 0 and the value set by self.max_damage.'''
-        # Pick a random value between 0 and self.max_damage
-        random_value = random.randint(0, self.max_damage)
-        return random_value
+        total_damage = 0
+        for ability in self.abilities:
+            total_damage += ability.attack()
+        return total_damage
+
+    def defend(self):
+        total_defense = 0
+        for armor in self.armors:
+            total_defense = armor.block()
+        return total_defense
+
+    def take_damage(self, damage):
+        damage_after_defense = damage - self.defend()
+        if damage_after_defense > 0:
+            self.current_health -= damage_after_defense
+
+    def is_alive(self):
+        if self.current_health > 0:
+            return True
+        else:
+            return False
+
+    def add_kills(self, num_kills=1):
+        self.kills += num_kills
+
+    def add_deaths(self, num_deaths=1):
+        self.deaths += num_deaths
+
+    def fight(self, opponent):
+        # Check if any of the Heroes can do damage
+        if self.abilities.count == 0 and opponent.abilities.count == 0:
+            return print("Battle Draw, because both out of moves")
+        while self.is_alive() and opponent.is_alive():
+            self_damage = self.attack()
+            opponent.take_damage(self_damage)
+            opponent_damage = opponent.attack()
+            self.take_damage(opponent_damage)
+
+        if self.is_alive() == False and opponent.is_alive() == False:
+            self.add_deaths()
+            opponent.add_deaths()
+            print("Battle Draw, beacause both died")
+
+        elif self.is_alive() == False:
+            self.add_deaths()
+            opponent.add_kills()
+            print(f"{opponent.name} won the Battle")
+
+        elif opponent.is_alive() == False:
+            self.add_kills()
+            opponent.add_deaths()
+            print(f"{self.name} won the Battle")
+        else:
+            return('error')
 
 
 if __name__ == "__main__":
-    my_hero = Hero("Grace Hopper", 200)
-    print(my_hero.name)
-    print(my_hero.current_health)
+    # If you run this file from the terminal
+    # this block is executed.
 
-    my_hero.fight('hero2')
-
-    ability = Ability("Debugging Ability", 20)
-    print(ability.name)
-    print(ability.attack())
+    hero1 = Hero("Wonder Woman")
+    hero2 = Hero("Dumbledore")
+    ability1 = Ability("Super Speed", 300)
+    ability2 = Ability("Super Eyes", 130)
+    ability3 = Ability("Wizard Wand", 300)
+    ability4 = Ability("Wizard Beard", 130)
+    hero1.add_ability(ability1)
+    hero1.add_ability(ability2)
+    hero2.add_ability(ability3)
+    hero2.add_ability(ability4)
+    hero1.fight(hero2)
